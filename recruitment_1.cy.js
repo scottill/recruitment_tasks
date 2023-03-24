78b761dd4af95225635cc9ee610039e23a07b966
scottill <128103535+scottill@users.noreply.github.com>
2023-03-24 05:35:16 +0100

    task corrections

--- recruitment_1.cy.js
+++ recruitment_1.cy.js
@@ -1,12 +1,15 @@
 describe('form spec', () => {
 
+    const confirmPass = Math.random().toString(20).substring(2, 10)
+
     beforeEach(() => {
         cy.visit(' https://discover.arx.city/auth?signup=true')
+        cy.get('[data-testid="form:first-name"]').type("John")
         cy.get('[data-testid="form:last-name"]').type("Doe")
         cy.get('[data-testid="form:work-email"]').type("johndoe@email.com")
         cy.get('[data-testid="form:phone-number"]').type("1234567890")
         cy.get('[data-testid="form:company-name"]').type("company")
-        cy.get('[data-testid="form:password"]').type("")
+        cy.get('[data-testid="form:password"]').type(Cypress.env('password'))
     })
 
     afterEach(() => {
@@ -14,18 +17,18 @@ describe('form spec', () => {
         cy.get('[data-testid="form:alert"]').should('exist')
     })
 
-    it('a', () => {
-        cy.get('[data-testid="form:first-name"]').type("John")
-        cy.get('[data-testid="form:confirm-password"]').type("")
+    it('User doesn’t check terms and condition checkbox', () => {
+        cy.get('[data-testid="form:confirm-password"]').type(Cypress.env('password'))
     })
 
-    it('b', () => {
-        cy.get('[data-testid="form:confirm-password"]').type("")
+    it('User doesn’t provide any input in “First name” field', () => {
+        cy.get('[data-testid="form:confirm-password"]').type(Cypress.env('password'))
         cy.get('[data-testid="form:checkbox"]').click()
+        cy.get('[data-testid="form:first-name"]').clear()
     })
 
-    it('c', () => {
-        cy.get('[data-testid="form:confirm-password"]').type("")
+    it('User password and confirm password field have mismatching inputs', () => {
+        cy.get('[data-testid="form:confirm-password"]').type(confirmPass)
         cy.get('[data-testid="form:checkbox"]').click()
     })
 })
